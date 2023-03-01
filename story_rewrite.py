1e38ce97583b5ae176d08dbe75eedbc72bf713c8
ZHANGXINGYU <1018995004@qq.com>
2023-03-01 15:43:16 +0800

    api works

--- story_rewrite.py
+++ story_rewrite.py
@@ -1,15 +1,13 @@
-from revChatGPT.revChatGPT import Chatbot
 from config import config
-from app import ChatApplication
+# from app import ChatApplication
 from utils import print_logo, print_warp, error, input_option
 from colorama import Fore
 
 PYCHATGPT_AVAILABLE = True
-
 try:
-    from pychatgpt import Chat, Options, OpenAI
+    from revChatGPT.V1 import Chatbot
 except:
-    error("你的Python版本低于3.9或没有安装pychatgpt，无法使用登陆功能，将使用默认token。\n\n\n")
+    error("你没有安装revChatGPT，无法使用。\n\n\n")
     PYCHATGPT_AVAILABLE = False
 
 
@@ -26,90 +24,51 @@ class StoryTeller:
             config = {
                 "email": str,
                 "password": str,
-                "options": {
-                    "log": bool,
-                    "track": bool,
-                    "proxies": str,
-                    # optional, pass a file path to save the conversation
-                    "chat_log": str,
-                    "id_log": str
-                },
-                "previous_convo_id": str # resume a conversation
             }
         """
         self.background = background
-        self.type = None  # 0: revChatGPT 1: PyChatGPT
+        self.type = None  # session token or openai account
         self.config = None
         self.chatbot = None
         self.first_interact = True
 
-    def login_by_token(self):
-        self.chatbot = Chatbot(self.config, conversation_id=None)
-        self.chatbot.reset_chat()  # Forgets conversation
-        self.chatbot.refresh_session()  # Uses the session_token to get a new bearer token
-
-    def login_by_account(self):
-        previous_convo_id = None
-        options = Options()
-        options.log = self.config['options']['log']
-        options.track = self.config['options']['track']
-        if 'proxies' in self.config['options'].keys():
-            options.proxies = self.config['options']['proxies']
-        if 'previous_convo_id' in self.config.keys():
-            previous_convo_id = self.config['previous_convo_id']
-        self.chatbot = Chat(email=self.config['email'], password=self.config['password'], options=options,
-                            previous_convo_id=previous_convo_id)
+    def _login(self, _config):
+        self.chatbot = Chatbot(_config)
 
     def config_by_token(self):
+        """
+        Config by session token
+        """
         self.type = 0
-        expired_creds = OpenAI.token_expired()
-        expired_creds = True
 
-        if expired_creds:
-            print("access_token过期，请选择登陆(y)或者使用默认的session_token(n)。请输入(y/n):")
-            _input = input()
-            if _input == 'y':
-                email = input("邮箱：")
-                pwd = input("密码：")
-                open_ai_auth = OpenAI.Auth(email_address=email, password=pwd)
-                try:
-                    open_ai_auth.create_token()
-                except:
-                    print("登陆失败！请检查邮箱和密码。将使用默认session_token进入。")
-                    return config
-                else:
-                    access_token = OpenAI.get_access_token()
-                    _config = {"Authorization": access_token[0]}
-                    return _config
-            else:
-                return config
-        else:
-            access_token = OpenAI.get_access_token()
-            _config = {"Authorization": access_token[0]}
-            return _config
+        session_token = config["session_token"]
+        print(session_token)
+        _config = {"session_token": session_token}
+        return _config
 
     def config_by_account(self):
+        """
+        Config by Openai account
+        """
         _config = {
             "email": "",
             "password": "",
-            "options": {
-                "log": True,
-                "track": True,
-                # optional, pass a file path to save the conversation
-                # "chat_log": None,
-                # "id_log": None
-            },
-            # "previous_convo_id": str  # resume a conversation
+            # "conversation_id": "",
+            # "parent_id": "",
+            # "proxy": "",
+            "paid": False
         }
         self.type = 1
-        print("请输入邮箱密码。")
+        print("请输入OpenAI帐号的邮箱及密码。")
         _config["email"] = input("邮箱:")
         _config["password"] = input("密码:")
-        _config["options"]["log"] = input_option("是否开启log(方便debug):", 'y', 'n', 'n')
-        _config["options"]["track"] = input_option("是否开启对话追踪(保存对话并下次能继续对话):", 'y', 'n', 'y')
         prox = input("若使用代理，请输入代理url。不使用则直接回车。")
         if prox:
-            _config["options"]["proxies"] = prox
+            _config["proxy"] = prox
+
+        paid = input_option("你是否为付费用户？", 'y', 'n', 'n')
+        if paid:
+            _config["paid"] = True
         try:
             with open('id_log.txt', 'r') as f:
                 lines = f.readlines()
@@ -118,17 +77,15 @@ class StoryTeller:
                     resume = input_option("发现之前的冒险。是否继续冒险:", 'y', 'n', 'y')
                     if resume:
                         self.first_interact = False
-                        _config["previous_convo_id"] = last_line
+                        _config["parent_id"] = last_line
         except:
             pass
+
         return _config
 
     def setup_chatbot(self):
         self.config = self.get_config()
-        if self.type:
-            self.login_by_account()
-        else:
-            self.login_by_token()
+        self._login(self.config)
 
         if self.first_interact:
             print("请输入背景故事。置空则使用默认背景故事。")
@@ -141,32 +98,41 @@ class StoryTeller:
                     lines = f.readlines()
                     last_line = lines[-1]
                     if last_line:
-                        self.background = last_line[10:]
+                        self.background = last_line
             except:
                 self.background = ""
         print("\n\n\n")
 
     def get_config(self):
-        if PYCHATGPT_AVAILABLE:
-            print("请选择使用方式：\n y:登陆使用。(需要OpenAI账号。能够使用代理，读取故事，保存故事等功能) \n n:不登陆使用。("
-                  "使用之前获取的access_token或者默认的session_token运行)")
-            res = input()
-            if res == 'y':
-                return self.config_by_account()
-            else:
-                return self.config_by_token()
-        else:
-            self.type = 0
-            return config
+        # if PYCHATGPT_AVAILABLE:
+        #     print("请选择使用方式：\n y:登陆使用。(需要OpenAI账号) \n n:不登陆使用。("
+        #           "使用默认的session_token运行，可能无法运行)")
+        #     res = input()
+        #     if res == 'y':
+        #         return self.config_by_account()
+        #     else:
+        #         return self.config_by_token()
+        # else:
+        #     self.type = 0
+        #     return config
+        return self.config_by_account()
 
     def start_cli(self):
         print_logo()
         self.setup_chatbot()
         self.interactive()
 
-    def start_app(self):
-        app = ChatApplication(self.background)
-        app.run()
+    # def start_app(self):
+    #     app = ChatApplication(self.background)
+    #     app.run()
+
+    def save_conversation_id(self, conv_id):
+        with open('id_log.txt', 'w') as f:
+            f.writelines(conv_id)
+
+    def save_conversations(self, res):
+        with open('chat_log.txt', 'w') as f:
+            f.writelines(res)
 
     def action(self, user_action):
         if user_action[-1] != "。":
@@ -174,16 +140,22 @@ class StoryTeller:
         if self.first_interact:
             prompt = """现在来充当一个冒险文字游戏，描述时候注意节奏，不要太快，仔细描述各个人物的心情和周边环境。一次只需写四到六句话。
             开头是，""" + self.background + """ 你""" + user_action
-            self.first_interact = False
         else:
             prompt = """继续，一次只需要续写四到六句话，总共就只讲5分钟内发生的事情。
             你""" + user_action
-        if self.type:
-            response, _, _ = self.chatbot.ask(prompt)
-        else:
-            resp = self.chatbot.get_chat_response(prompt)  # Sends a request to the API and returns the response by
-            # OpenAI
-            response = resp["message"]
+        response = ""
+
+        for data in self.chatbot.ask(
+            prompt
+        ):
+            response = data["message"]
+
+        self.save_conversations(response)
+
+        if self.first_interact:
+            self.first_interact = False
+            self.save_conversation_id(self.chatbot.get_conversations()[0]['id'])
+
         return response
 
     def interactive(self):
--- test.py
+++
@@ -1,4 +0,0 @@
-prox = input("若使用代理，请输入代理url。不使用则直接回车。")
-if prox:
-    print(1)
-print(prox)
